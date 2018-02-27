b6843887bff18d4718dd9350e4beb8c2b05f51fb
Karl Isenberg <karlkfi@gmail.com>
2018-02-27 00:04:00 -0800

    Mount AWS config dir (~/.aws)
    - Make AWS env vars optional

--- aws.sh
+++ aws.sh
@@ -7,11 +7,17 @@ set -o pipefail
 # enable interruption signal handling
 trap - INT TERM
 
+if [[ ! -d "${HOME}/.aws" ]]; then
+	mkdir -p "${HOME}/.aws"
+	chown -R ${UID}:${GID} "${HOME}/.aws"
+fi
+
 docker run --rm \
 	-t $(tty &>/dev/null && echo "-i") \
-	-e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
-	-e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
-	-e "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" \
+	$([[ -n "${AWS_ACCESS_KEY_ID:-}" ]] && echo "-e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" || true) \
+	$([[ -n "${AWS_SECRET_ACCESS_KEY:-}" ]] && echo "-e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" || true) \
+	$([[ -n "${AWS_DEFAULT_REGION:-}" ]] && echo "-e AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" || true) \
 	-v "$(pwd):/project" \
+	-v "${HOME}/.aws:/root/.aws"
 	mesosphere/aws-cli \
 	"$@"
